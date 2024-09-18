15276ddf443879c876079e8a110be4c97ab5ed05
Adarsh <ashu2100ag@gmail.com>
2024-09-18 21:03:15 +0530

    changed attribute

--- src/Components/HeroSection-components/Video.jsx
+++ src/Components/HeroSection-components/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Video({video}) {
   return (
     <>
-      <video src={video} autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' type="video/mp4">
+      <video src={video} autoPlay preload='auto' loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' type="video/mp4">
             Your browser does not support the video tag.
       </video>
     </>
